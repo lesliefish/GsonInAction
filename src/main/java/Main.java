c19cee0a43d2c191897a27f2f6fe0a86c5c04898
yu <yuleics@outlook.com>
2019-04-05 23:51:42 +0800

    first application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import com.lesliefish.gson.t01firstapp.FirstApplicationTester;
+
 public class Main {
 
     public  static void main(String[] args){
         System.out.println("Gson in action.");
+
+        FirstApplicationTester.test();
     }
 }
